f40b5f9ab21e8b04d3e64d4eed9f4c074dc1046d
Albert Chaharbakhshi <achaharbakhshi@wellsgardner.com>
2014-09-05 17:41:34 -0500

    Fixed problem with Box2DHelper.java not being under source control.
    Updated .gitignore to omit Windows' thumbnail files.

--- core/src/com/wge/G2E2014/helpers/Box2DHelper.java
+++ core/src/com/wge/G2E2014/helpers/Box2DHelper.java
@@ -4,9 +4,13 @@ package com.wge.G2E2014.Helpers;
  * Created by achaharbakhshi on 9/4/2014.
  */
 public class Box2DHelper {
-    private static final float PPM = 100; //pixels per meter}
+    private static final float PPM = 200; //pixels per meter}
 
-    public static final float PPM(float inputPixels) {
+    public static final float PixelsToMeters(float inputPixels) {
         return inputPixels / PPM;
+    }
+
+    public static final float MetersToPixels(float inputMeters) {
+        return inputMeters * PPM;
     }
 }
